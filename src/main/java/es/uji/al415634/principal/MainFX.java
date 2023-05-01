424b9fcb5e524c7a0742d0ad6de1a9eb24c882bc
RJLei <al415673@uji.es>
2023-05-01 22:18:04 +0200

    Interfaz gráfica

--- src/main/java/es/uji/al415634/principal/MainFX.java
+++ src/main/java/es/uji/al415634/principal/MainFX.java
@@ -1,11 +1,22 @@
 package es.uji.al415634.principal;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
 
 public class MainFX extends Application {
 
@@ -14,7 +25,71 @@ public class MainFX extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setTitle("Song Recommender");
+
+        //primer apartado
+        ToggleGroup grupo1 = new ToggleGroup();
+        Label titulo1 = new Label("Recommendation Type");
+        RadioButton knn = new RadioButton("Recommended based on song features");
+        RadioButton kmeans = new RadioButton("Recommend based on guessed genre");
+        VBox caja1 = new VBox(titulo1,knn, kmeans);
+        //Ajustes de la caja1
+        caja1.setSpacing(10);
+        caja1.setPadding(new Insets(10));
+        //Asignarle grupo
+        knn.setToggleGroup(grupo1);
+        kmeans.setToggleGroup(grupo1);
+
+        //segundo apartado
+        ToggleGroup grupo2 = new ToggleGroup();
+        Label titulo2 = new Label("Distance Type");
+        RadioButton eucl = new RadioButton("Euclidean");
+        RadioButton manh = new RadioButton("Manhattan");
+        VBox caja2 = new VBox(titulo2,eucl, manh);
+        //Ajustes de la caja2
+        caja2.setSpacing(10);
+        caja2.setPadding(new Insets(10));
+        //Asiganarle grupo
+        eucl.setToggleGroup(grupo2);
+        manh.setToggleGroup(grupo2);
+
+        //tercer apartado
+        Label titulo3 = new Label("Song Titles");
+        ObservableList<String> canciones = FXCollections.observableArrayList();
+        //Añadir titulos de canciones
+        addTitleSong(canciones);
+        //Poner el scrollPanel
+        ListView<String> lista = new ListView<>(canciones);
+        VBox caja3 = new VBox(titulo3,lista);
+        //Ajustes de la caja3
+        caja3.setSpacing(10);
+        caja3.setPadding(new Insets(10));
+
+        //cuarto apartado
+        Button recommend = new Button("Recommend...");
+        VBox caja4 = new VBox(recommend);
+        //Ajustes de la caja4
+        caja4.setSpacing(10);
+        caja4.setPadding(new Insets(10));
+
+
+        //Fusionar
+        GridPane root = new GridPane();
+        root.setPadding(new Insets(10,10,10,10));
+        root.setVgap(5);
+        root.setHgap(5);
+        root.setAlignment(Pos.TOP_LEFT);
+        //Posicionar los elementos
+        root.add(caja1, 0,0);
+        root.add(caja2, 0,1);
+        root.add(caja3, 0,2);
+        root.add(caja4,0,3);
+
+        //Mostrarlo
+        primaryStage.setScene(new Scene(root, 400, 800));
+        primaryStage.show();
+
         /*primaryStage.setTitle("Hello World!");
         StackPane root = new StackPane();
         //Button btn = new Button("Hola");
@@ -36,7 +111,7 @@ public class MainFX extends Application {
         primaryStage.setScene(new Scene(root1, 250, 250));
         primaryStage.show();*/
 
-        ToggleGroup grupo2 = new ToggleGroup();
+        /*ToggleGroup grupo2 = new ToggleGroup();
         primaryStage.setTitle("Distance Type");
         StackPane root2 = new StackPane();
         RadioButton eucl = new RadioButton("Euclidean");
@@ -48,7 +123,15 @@ public class MainFX extends Application {
         eucl.setToggleGroup(grupo2);
         manh.setToggleGroup(grupo2);
         primaryStage.setScene(new Scene(root2, 250, 250));
-        primaryStage.show();
+        primaryStage.show();*/
+    }
 
+    private ObservableList<String> addTitleSong(ObservableList<String> lista) throws IOException {
+        BufferedReader buffer = new BufferedReader(new FileReader("src/main/java/es/uji/al415634/Files/songs_train_names.csv"));
+        String cadena;
+        while((cadena= buffer.readLine())!= null){
+            lista.add(cadena);
+        }
+        return lista;
     }
 }
--- src/test/java/es/uji/al415634/principal/CSVTest.java
+++ src/test/java/es/uji/al415634/principal/CSVTest.java
@@ -1,13 +1,8 @@
 package es.uji.al415634.principal;
 
-import es.uji.al415634.principal.Lectura.CSVLabeledFileReader;
-import es.uji.al415634.principal.Lectura.CSVUnlabeledFileReader;
-import es.uji.al415634.principal.Lectura.ReaderTemplate;
-import es.uji.al415634.principal.Tablas.Row;
+import es.uji.al415634.principal.Lectura.CSV;
 import es.uji.al415634.principal.Tablas.RowWithLabel;
-import es.uji.al415634.principal.Tablas.Table;
 import es.uji.al415634.principal.Tablas.TableWithLabels;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -19,51 +14,30 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CSVTest {
     //Para la prueba 5
-    private Collection<List<Double>> pruebasConL;
-    private final Integer[] lineasConL = {8, 18, 48, 58, 88, 128};
-    private final Double[] vect1 = {4.4, 2.9, 1.4, 0.2}; //linea 8
-    private final Double[] vect2 = {5.7, 3.8, 1.7, 0.3}; //linea 18
-    private final Double[] vect3 = {5.3, 3.7, 1.5, 0.2}; //linea 48
-    private final Double[] vect4 = {6.6, 2.9, 4.6, 1.3}; //linea 58
-    private final Double[] vect5 = {5.6, 3.0, 4.1, 1.3}; //linea 88
-    private final Double[] vect6 = {6.4, 2.8, 5.6, 2.1}; //linea 128
-
-    private Collection<List<Double>> pruebasSinL;
-    private final Integer[] lineasSinL = {3, 6, 9, 10, 11};
-    private final Double[] v1 = {4.6,3.1,1.5}; //linea 3
-    private final Double[] v2 = {6.9,3.1,4.9}; //linea 6
-    private final Double[] v3 = {5.8,2.7,5.1}; //linea 9
-    private final Double[] v4 = {7.1,3.0,5.9}; //linea 10
-    private final Double[] v5 = {6.3,2.9,5.6}; //linea 11
-
-
-
-    @BeforeEach
+    private Collection<List<Double>> pruebas;
+    private final Integer[] lineas = {8,18,48,58,88,128};
+    private final Double[] vect1 = {4.4,2.9,1.4,0.2}; //linea 8
+    private final Double[] vect2 = {5.7,3.8,1.7,0.3}; //linea 18
+    private final Double[] vect3 = {5.3,3.7,1.5,0.2}; //linea 48
+    private final Double[] vect4 = {6.6,2.9,4.6,1.3}; //linea 58
+    private final Double[] vect5 = {5.6,3.0,4.1,1.3}; //linea 88
+    private final Double[] vect6 = {6.4,2.8,5.6,2.1}; //linea 128
+    @org.junit.jupiter.api.BeforeEach
     void setUp() {
-        //Para la prueba con labels
-        pruebasConL = new ArrayList<>();
+        //Para la prueba5
+        pruebas=new ArrayList<>();
         //Añadir las listas a la lista de listas
-        pruebasConL.add(addElement(vect1));
-        pruebasConL.add(addElement(vect2));
-        pruebasConL.add(addElement(vect3));
-        pruebasConL.add(addElement(vect4));
-        pruebasConL.add(addElement(vect5));
-        pruebasConL.add(addElement(vect6));
-
-        //Para la prueba sin labels
-        pruebasSinL = new ArrayList<>();
-        //Añadir las listas a la lista de listas
-        pruebasSinL.add(addElement(v1));
-        pruebasSinL.add(addElement(v2));
-        pruebasSinL.add(addElement(v3));
-        pruebasSinL.add(addElement(v4));
-        pruebasSinL.add(addElement(v5));
+        pruebas.add(addElement(vect1));
+        pruebas.add(addElement(vect2));
+        pruebas.add(addElement(vect3));
+        pruebas.add(addElement(vect4));
+        pruebas.add(addElement(vect5));
+        pruebas.add(addElement(vect6));
     }
-
     //El addElement añade todos los elementos de la lista
-    private List<Double> addElement(Double[] vect) {
+    private List<Double> addElement(Double[] vect){
         List<Double> lista = new ArrayList<>();
-        for (int i = 0; i < vect.length; i++) {
+        for(int i=0; i<vect.length;i++){
             lista.add(vect[i]);
         }
         return lista;
@@ -75,66 +49,39 @@ class CSVTest {
 
     @Test
     void readTableLabels() throws IOException {
-        ReaderTemplate<Table> csv = new CSVLabeledFileReader("src/main/java/es/uji/al415634/Files/Iris");
-        TableWithLabels tablaConL = (TableWithLabels) csv.readTableFromSource();
+        CSV csv = new CSV();
+        TableWithLabels tabla = csv.readTableLabels("src/main/java/es/uji/al415634/Files/Iris");
         //Comprobar num rows
-        assertEquals(150, tablaConL.listaRows.size());
+        assertEquals(150, tabla.listaRows.size());
 
         //Comprobar num columnas headers
-        assertEquals(5, tablaConL.headers.size());
+        assertEquals(4, tabla.headers.size());
 
         //Comprobar etiquetas headers
-        assertEquals("sepal length", tablaConL.headers.get(0));
-        assertEquals("sepal width", tablaConL.headers.get(1));
-        assertEquals("petal length", tablaConL.headers.get(2));
-        assertEquals("petal width", tablaConL.headers.get(3));
-        assertEquals("class", tablaConL.headers.get(4));
+        assertEquals("sepal length", tabla.headers.get(0));
+        assertEquals("sepal width", tabla.headers.get(1));
+        assertEquals("petal length", tabla.headers.get(2));
+        assertEquals("petal width", tabla.headers.get(3));
 
         //El número que se asigna a cada fila es correcto
-        for (int i = 1; i < tablaConL.listaRows.size(); i++) {
-            if (i < 50) {
-                assertEquals(0, tablaConL.getRowAt(i).getNumberClass());
-            } else if (i < 100) {
-                assertEquals(1, tablaConL.getRowAt(i).getNumberClass());
+        for(int i = 1; i<tabla.listaRows.size(); i++){
+            if(i<50){
+                assertEquals(0, tabla.getRowAt(i).getNumberClass());
             } else
-                assertEquals(2, tablaConL.getRowAt(i).getNumberClass());
+            if (i<100) {
+                assertEquals(1, tabla.getRowAt(i).getNumberClass());
+            }else
+                assertEquals(2, tabla.getRowAt(i).getNumberClass());
         }
 
         //Las filas que guardas en una tabla puedes recuperarlas conteniendo los mismos valores que guardaste
-        int j = 0;
-        for (List<Double> aComprobar : pruebasConL) {
-            RowWithLabel fila = tablaConL.getRowAt(lineasConL[j]);
+        int j=0;
+        for(List<Double> aComprobar: pruebas){
+            RowWithLabel fila = tabla.getRowAt(lineas[j]);
             assertEquals(aComprobar.get(0), fila.getData().get(0));
             assertEquals(aComprobar.get(1), fila.getData().get(1));
             assertEquals(aComprobar.get(2), fila.getData().get(2));
             assertEquals(aComprobar.get(3), fila.getData().get(3));
-            j++;
-        }
-    }
-
-    @Test
-    void readTable() throws IOException {
-        ReaderTemplate<Table> csv1 = new CSVUnlabeledFileReader("src/main/java/es/uji/al415634/Files/Prueba1");
-        Table tablaSinL = csv1.readTableFromSource();
-        //Comprobar num rows
-        assertEquals(12, tablaSinL.listaRows.size());
-
-        //Comprobar num columnas headers
-        assertEquals(3, tablaSinL.headers.size());
-
-        //Comprobar etiquetas headers
-        assertEquals("x", tablaSinL.headers.get(0));
-        assertEquals("y", tablaSinL.headers.get(1));
-        assertEquals("z", tablaSinL.headers.get(2));
-
-
-        //Las filas que guardas en una tabla puedes recuperarlas conteniendo los mismos valores que guardaste
-        int j = 0;
-        for (List<Double> aComprobar : pruebasSinL) {
-            Row fila = tablaSinL.getRowAt(lineasSinL[j]);
-            assertEquals(aComprobar.get(0), fila.getData().get(0));
-            assertEquals(aComprobar.get(1), fila.getData().get(1));
-            assertEquals(aComprobar.get(2), fila.getData().get(2));
             j++;
         }
     }
